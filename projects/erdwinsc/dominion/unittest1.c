81589403199b525571f38a59cf1aa3f9ad1d737c
Chad_Erdwins <cerdwins7@gmail.com>
2017-10-19 15:04:32 -0400

    unit Testing
    worked on some unit testing for various functions in dominion.c

--- projects/erdwinsc/dominion/unittest1.c
+++ projects/erdwinsc/dominion/unittest1.c
@@ -1,12 +1,52 @@
+/*
+unittest1.c
+Chad Erdwins
+testing numHandCount() for bugs
+CS362
+*/
+
 #include "dominion.h"
 #include "dominion_helpers.h"
-#include "rngs.h"
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 
-main()
-{
-    printf("Hello World");
+//this variable should remain 0 if the test doesn't find bugs
+int failure = 0;
 
+//my personal assertion that accepts 2 parameters, cardsInHand and a error message
+void personalAssert(int choya, char* errorMSG) {
+	if (choya == 0) {
+		printf("Bug Detected: %s\n", errorMSG);
+		//set failure = 1, means a bug has been detected
+		failure = 1;
+	}
+}
+
+int reviewAssertions() {
+	if (!failure) {
+		printf("No bugs detected in  function numHandCount.\n");
+		return 0;
+	}
+	return 1;
+}
+
+int main() {
+	//need to intiate the game state in order to begin
+	struct gameState game1;
+
+	//making an array that represents the cards possible
+	int gooArray[10] = { smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
+		outpost,baron,tribute };
+
+	//calling function initializeGame to get things started, passing in the array of cards previously defined
+	//2 is the number of players, gooArray is the hand, and 3 is the seed number, game1 is the game state
+	initializeGame(2, gooArray, 3, &game1);
+
+	//assigning number of cards to a variable
+	int cardsInHand = game1.handCount[whoseTurn(&game1)];
+
+	//
+	personalAssert(numHandCards(&game1) == cardsInHand, "There is a bug in numHandCount because the wrong value was returned!");
+
+	return reviewAssertions();
 }
---
+++ projects/erdwinsc/dominion/unittest2.c
@@ -0,0 +1,58 @@
+/*
+Chad Erdwins CS 362
+unittest2.c
+testing the function initializeGame() in dominion.c
+*/
+
+#include "dominion.h"
+#include <stdio.h>
+
+//this variable should remain 0 if all goes smoothly
+int critFail = 0;
+
+void verifyAssert() {
+	if (!critFail) {
+		printf("NO BUGS DETECTED!\n");
+	}
+	else {
+		printf("WARNING! BUGS ARE PRESENT!\n");
+	}
+}
+
+void personalAssertion(int choya, const char *errorMsg) {
+	if (!choya) {
+		printf("Failure: %s\n", errorMsg);
+		//bugs are present
+		critFail = 1;
+	}
+}
+
+int main() {
+	// Declaring variables so game will begin
+	int cardsAvailable[] = { smithy,adventurer,gardens,embargo,cutpurse,mine,
+		ambassador,outpost,baron,tribute };
+	struct gameState gameOn;
+	//number of players available
+	int participants = 2;
+	//random seed number
+	int rngSeed = 4975;
+	
+
+	//calling initializeGame from dominion.c and passing the necessary parameters
+	int payLoad = initializeGame(participants, cardsAvailable, rngSeed, &gameOn);
+
+	//calling my assertion to debug and verify
+	personalAssertion((payLoad == 0), "Initializing Game Failure");
+
+	// declaring a deck with many duplicates to test and see if it detects bug
+	int duplicateDeck[] = { smithy,smithy,adventurer,adventurer,gardens,gardens,
+		embargo,embargo,cutpurse,cutpurse };
+
+	//passing everything to initializeGame function and the duplicate array
+	payLoad = initializeGame(participants, duplicateDeck, rngSeed, &gameOn);
+
+	//I expect this message to return because of the multiple duplicates
+	personalAssertion((payLoad != 0), "Multiple Duplicates Detected!!!");
+
+	verifyAssert();
+}
